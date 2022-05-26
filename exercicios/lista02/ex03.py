24a4bf32daf0a9dc635bee84c73c5c924c5ec8a7
lccfonseca <lccfonseca@gmail.com>
2022-05-26 17:22:39 -0300

    Exercicio 03 da Lista 02

--- exercicios/lista02/ex03.py
+++ exercicios/lista02/ex03.py
@@ -1,6 +1,7 @@
 import copy
 
-from edb.basico.lista import ListaE
+from edb.basico.lista import Node
+from edb.basico.lista import ListaEncadeada
 
 # Tutoriais Listas Encadeadas
 # https://realpython.com/linked-lists-python/
@@ -15,9 +16,38 @@ from edb.basico.lista import ListaE
 # contém o nome buscado para o início da lista, de forma que ele possa ser
 # encontrado mais rapidamente na próxima vez que for buscado.
 
-class ListaEx03(ListaE):
-    def __init__(self):
-        pass
+class ListaEx03(ListaEncadeada):
+    def __init__(self, nodes=None):
+        super().__init__(nodes)
+
+    def search(self, node):
+        try:
+            l.remove_node(node.data)
+            l.add_first(node)
+            print(f"Site: {node.data} encontrado e priorizado!")
+        except Exception:
+            print("Erro: Nó não encontrado!")
+
+
+    def show_sites(self):
+        for node in self:
+            print(node.data)
+
 
 if __name__ == '__main__':
     print('Exercício 03 da Lista de Exercícios 02')
+
+    l = ListaEx03(["www.jp.com.br"])
+
+    l.add_last(Node("www.google.com"))
+    l.add_last(Node("www.cnn.com"))
+    l.add_last(Node("www.fox.com"))
+    l.add_last(Node("www.uol.com.br"))
+    l.add_last(Node("g1.globo.com"))
+
+    l.show_sites()
+
+    l.search(Node("www.cnn.com"))
+    l.search(Node("xyz.com"))
+
+    l.show_sites()
